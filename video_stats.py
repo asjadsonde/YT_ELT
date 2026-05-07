03799f2d121000a86ce4404416cce81cd7abdd43
root <root@66-116-237-34.webhostbox.net>
2026-05-06 22:04:52 -0600

    Updated script to get data of each video

--- video_stats.py
+++ video_stats.py
@@ -35,8 +35,6 @@ try:
 
         channel_playlistId = channel_items["contentDetails"]["relatedPlaylists"]["uploads"]
 
-        print(channel_playlistId)
-
         return channel_playlistId
 
 except requests.exceptions.RequestException as e:
@@ -82,6 +80,62 @@ try:
 except requests.exceptions.RequestException as e:
     raise e
 
+
+
+try:
+    def extract_video_data(video_ids):
+
+        extracted_data = []
+
+        def batch_list(video_id_list, batch_size):
+            for video_id in range(0,len(video_id_list),batch_size):
+                yield video_id_list[video_id:video_id + batch_size]
+
+        for batch in batch_list(video_ids,maxResults):
+            batch_str = ",".join(batch)
+
+            url = f"https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={batch_str}&key={API_KEY}"
+            
+            response = requests.get(url)
+            
+            #Raise exception for error during get request
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get("items"):
+
+                video_id = item["id"]
+                statisitcs = item["statistics"]
+                snippet = item["snippet"]
+                contentDetails = item["contentDetails"]
+
+                video_data = {
+                        "id":video_id,
+                        "title":snippet["title"],
+                        "publishedAt":snippet["publishedAt"],
+                        "duration":contentDetails["duration"],
+                        "viewCount":data.get("viewCount"),
+                        "likeCount":data.get("likeCount"),
+                        "commentCount":data.get("commentCount")
+                        }
+                
+                extracted_data.append(video_data)
+
+        return extracted_data
+
+                    
+
+
+
+except requests.exceptions.RequestException as e:
+    raise e
+
+
+
+
 if __name__ == "__main__":
     playlistId = get_playlist_Id()
-    get_video_ids(playlistId)
+    video_ids = get_video_ids(playlistId)
+    print(extract_video_data(video_ids))
+
